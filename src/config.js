7a6227c55f498ec1ce30ec77198a77ee88f30538
kingsman20 <kingsley@nextravel.com>
2019-10-26 19:31:21 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "kingslife-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: " https://etcngc90ab.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_grVkVkJmH",
+    APP_CLIENT_ID: "6qtfsmuf3g0el3tiha2tls0p2k",
+    IDENTITY_POOL_ID: "us-east-2:50ef5cea-e798-4bf1-bb66-4eab36a69402"
+  }
+};
